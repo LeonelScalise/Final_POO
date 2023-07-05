a3b6574ac3bd9c196db5189047c63c0d7293fffb
LeonelScalise <lscalise@itba.edu.ar>
2023-07-05 18:29:36 -0300

    crear Paquete individual

--- claseRouter.py
+++ claseRouter.py
@@ -10,13 +10,23 @@ class Router():
         self.retransmiciones_pendientes = Cola() # Retrasmiciones pendientes debe ser una cola
         self.retransmiciones = [] # Considerar que restransmiciones sea un contador y no una lista
         self.recepciones = []
-        self.siguiente=None
-        self.anterior=None
+        self.siguiente = None
+        self.anterior = None
         self.paquetes_enviados = [] # Considerar que paquetes_enviados sea un contador y no una lista
         
-    def crearPaquete(self, mensaje, destino):
-        id
-        newPaquete = Paquete()
+    def crearPaquete(self, mensaje:str, destino):
+        
+        if len(self.paquetes_enviados) == 0:
+            id = 1
+        else:
+            id = self.paquetes_enviados[-1].id + 1
+        
+        hoy = datetime.now()
+        newPaquete = Paquete(mensaje, {"id": id, "fecha": hoy, "origen": self, "destino": destino} )
+        
+        self.paquetes_enviados.append(newPaquete)
+        
+        return newPaquete
 
 
     def ordenarPaquetes(self):
