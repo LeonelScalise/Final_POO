6cc18e5ca5612a94d4b0ef6fea1b9d145258abb0
LeonelScalise <lscalise@itba.edu.ar>
2023-07-04 21:09:36 -0300

    crear Router en simulador (a corregir)

--- RoutingSim.py
+++ RoutingSim.py
@@ -8,6 +8,22 @@ class RoutingSim():
     def __init__(self, duracion):
         self.duracion = duracion
         self.timer = None
+    
+    def agregarRouter(self, coordenada = 0):
+        if coordenada == 0:
+            nombre = "router_0"
+        else:
+            nombre = "router_" + str(coordenada)
+
+        estado = "AGREGADO"
+        newRouter = Router(nombre, estado)
+        newRouter.estado = "ACTIVO"
+
+        return newRouter
+    
+    def inhabilitarRouter(self, router):
+        router.estado == "INACTIVO"
+
 
     def terminarSimulacion(self):
         print("Simulación finalizada")
@@ -17,6 +33,10 @@ class RoutingSim():
     def iniciarSimulacion(self):
         self.timer = threading.Timer(self.duracion, self.terminarSimulacion) # Ejecuta la función "terminarSimulación" durante los segundos que "duracion" indique 
         self.timer.start()
+    
+        router0 = self.agregarRouter()
+        router1 = self.agregarRouter(1)
+        threading.Timer(20, lambda : self.inhabilitarRouter(router0)).start() # A los 20 segundos de arrancar la simulación se deshabilita el router0
 
         # Realizar acá las operaciones de simulación (creacion de routers, creacion de paquetes, transmision de paquetes (armado de Ruta), etc.)
 
